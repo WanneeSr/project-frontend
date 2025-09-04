8f17322878a04a6812d191a062906e1aeb609e4e
Jokjok <93991020+WanneeSr@users.noreply.github.com>
2025-09-05 02:36:02 +0700

    update

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,14 +1,8 @@
 <template>
-    <div class="h-screen flex flex-col">
-        <ViewsHeader />
-        <div class="flex w-full h-full">
-            <ViewsSidebar />
-            <div class="w-full h-full px-2 py-2 bg-[#F5F5F5]">
-                
-                    <slot/>
-                
-            </div>
+    
+        <div class="flex flex-rows">
+            <ViewsSidebar/>
+            <slot/>
         </div>
-    </div>
-
+    
 </template>
--- layouts/home.vue
+++ layouts/home.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="h-screen">
         <Navbar/>
+        <slot/>
     </div>
 </template>
