d5098d74cb48443a36d3cfeb7a69e331e27ba366
Jokjok <93991020+WanneeSr@users.noreply.github.com>
2025-09-05 02:35:39 +0700

    update

--- components/views/sidebar.vue
+++ components/views/sidebar.vue
@@ -40,28 +40,42 @@ const items = [
 ]
 
 
+
+const router = useRouter();
+const user = checkToken();
+const isLoggedIn = ref();
+
+const logout = () => {
+    console.log(user);
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    isLoggedIn.value = false;
+    router.push('/login') // redirect ไปหน้า login
+    
+}
+
+
 </script>
 <template>
-    <div class="min-w-[250px] h-[100vh] bg-white   ">
-        <div v-for="item in items"  class="bg-white w-full text-gray-700 font-medium  hover:bg-slate-200 ">
-            <nuxt-link :to="item.to" class="py-3 flex flex-rows items-center justify-start px-7 ">
+    <div class="bg-violet-500 h-screen">
+        <div class="">
+                <p class="text-2xl font-bold p-2 text-center py-5">Admin panel</p>
+        </div>
+        <div v-for="item in items"  class="w-[200px] bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 text-white font-semibold has-checked:text-violet-600">
+            
+            <nuxt-link :to="item.to" class=" py-3 flex flex-rows items-center justify-start px-7 ">
                 <UIcon :name="item.icon" class="py-2"/>
                 <p class="p-3">{{ item.label }}</p>
             </nuxt-link>
         </div>
-       
+       <div class="w-[200px] bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 text-white font-semibold has-checked:text-violet-600">
+         <nuxt-link to="/" @click="logout()" class=" py-3 flex flex-rows items-center justify-start px-7 ">
+            <UIcon name="i-lucide-lightbulb" class="size-5" />
+            <p>Logout</p>
+         </nuxt-link>
+       </div>
 
     </div>
 </template>
 
 
-<!-- 
-<style scoped>
-.is_expanded {
-    color: orangered;
-}
-
-.is_expanded span {
-    color: orangered;
-}
-</style> -->
