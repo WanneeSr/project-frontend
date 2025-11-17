68604ba264e60ccc01f4ec78dc5bfb3335ff2848
WanneeSr <93991020+WanneeSr@users.noreply.github.com>
2025-11-17 15:51:19 +0700

    upate

--- pages/admin/courses/[id].vue
+++ pages/admin/courses/[id].vue
@@ -2,5 +2,30 @@
 const route = useRoute()
 
 // When accessing /posts/1, route.params.id will be 1
+
+  
 console.log(route.params.id)
 </script>
+
+<templete>
+    <div>
+        <div>
+          <div>
+            1
+
+          </div>
+          <div>
+            2
+
+          </div>
+          <div>
+            3
+
+          </div>
+          <div>
+            4
+
+          </div>
+        </div>
+    </div>
+</templete>
